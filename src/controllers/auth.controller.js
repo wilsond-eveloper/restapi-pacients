d27fcbe8d620de1c9f84f130af4b99d64173d953
Wilson <wilson.dlgd0@gmail.com>
2025-04-07 21:38:58 -0600

    Login and logout added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -34,4 +34,45 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => res.send('Login')
+export const login = async (req, res) => {
+
+    const { username, password } = req.body;
+
+    try {
+        const userFound = await Staff.findOne({ username })
+        if (!userFound) {
+            return res.status(400).json({ error: 'Username required' })
+        }
+
+        // Verificar contraseña
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Credenciales incorrectas' });
+        }
+        
+        // Crear y firmar el token JWT
+        const token = await createAccessToken({ id: userFound._id })
+
+        res.cookie("token", token)
+        return res.json({
+            message: 'Login exitoso',
+            success: true,
+            userFound
+        })
+    } 
+    catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+    res.json(`The login was complete`)
+}
+
+export const logout = async (req, res) => {
+    res.cookie('token', "", {
+        expires: new Date(0)
+    })
+    return res.sendStatus(200)
+}
+
+export const profile = async (req, res) => {
+    return res.json('Profile')
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 import {Router} from 'express'
-import {login, register} from '../controllers/auth.controller.js'
+import {login, register, logout, profile} from '../controllers/auth.controller.js'
 
 const router = Router()
 
 router.post('/register', register)
 
 router.post('/login', login)
+
+router.post('/logout', logout)
+
+router.get('/profile', profile)
 
 export default router
